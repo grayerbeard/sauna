4cd6aae214ebb41d684f9878c02c3c12a2821920
David Torrens <djgtorrens@gmail.com>
2019-12-11 17:51:30 +0000

    forgot change also on cpomments

--- tmux_start.sh
+++ tmux_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/pi/fanshim
-echo looking to kill any old tmux fanshim session
+echo looking to kill any old tmux sauna session
 tmux kill-session -t sauna
-echo now new tmux fanshim session 
+echo now new tmux sauna session 
 tmux new-session -d -s sauna 'python3 sauna.py'
 
