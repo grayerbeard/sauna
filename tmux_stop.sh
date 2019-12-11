3e646cc5e1847e553b4cb27d481255791fcc3e19
David Torrens <djgtorrens@gmail.com>
2019-12-11 17:53:02 +0000

    editing so stops sauna session

--- tmux_stop.sh
+++ tmux_stop.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd /home/pi/fanshim
-echo looking to kill any old fanshim stress session
-tmux kill-session -t fanshim
-echo looking to kill any old tmux stress session
-tmux kill-session -t stress
+cd /home/pi/sauna
+echo looking to kill any old tmux sauna session
+tmux kill-session -t sauna
