0cc5bab76e37c4d6589c2348e0681e0da046c2b4
David Torrens <djgtorrens@gmail.com>
2019-12-12 15:22:12 +0000

    correcting if statement

--- sauna.py
+++ sauna.py
@@ -83,11 +83,11 @@ while (config.scan_count <= config.max_scans) or (config.max_scans == 0):
 		# Shutdown Logics
 		if temp > config.min_temp:
 			shut_down_logic_target_reached = True
-		if (control.throttle = 100) and shut_down_logic_target_reached and (temp < shut_down_logic_last_temp_reading)	
+		if (control.throttle == 100) and shut_down_logic_target_reached and (temp < shut_down_logic_last_temp_reading)	
 			shut_down_logic_count += 1
 		elif temp > config.min_temp:
 			shut_down_logic_count = 0
-		if  shut_down_logic_count > 10 :
+		if  shut_down_logic_count > 100 :
 			call("sudo shutdown -h now", shell=True)
 			
 		# Logging
